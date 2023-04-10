34dce8460a560c184732cba8429792dd324f6f07
Andrew Wang <Andrewruujin@gmail.com>
2023-04-09 21:30:08 -0400

    commit tree 839e716053424523d86e933d27d48405260416b1

---
+++ app/serializers/movie_serializer.rb
@@ -0,0 +1,9 @@
+class MovieSerializer < ActiveModel::Serializer
+  attributes :id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director, :summary
+
+  def summaries
+    movies = Movie.all
+    render json: movies, each_serializer: MovieSummarySerializer
+  end
+  
+end
